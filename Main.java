e292459855799626ee0616d8ebfbef40b9122448
praveenkumarAnagani5e7 <32009538+praveenkumarAnagani5e7@users.noreply.github.com>
2017-09-16 14:43:52 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+import Student.*;
+import StudentGroup.*;
+import StudentArray.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student st = new Student(01,"Harikrihna",new Date(1995,04,26),63.87);
+		Student st2 = new Student(02,"krihna",new Date(2000,01,06),63.68);
+		Student st3 = new Student(03,"Hari",new Date(1998,12,06),63.58);
+		Student st4 = new Student(04,"praveen",new Date(1997,07,07),63.56);
+		
+		
+	}
+
+}
